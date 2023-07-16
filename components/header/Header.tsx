cdfe355f4ff1de645aca7989614217131fab033d
Johnny <mycoside@gmail.com>
2023-07-17 00:49:40 +0900

    v.1

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -4,7 +4,7 @@ import styles from "./Header.module.scss";
 export default function Header() {
   return (
     <header className={styles.container}>
-      <h1 className={styles.title}>尹정권 임기 카운트다운dd</h1>
+      <h1 className={styles.title}>尹정권 임기 카운트다운</h1>
     </header>
   );
 }
